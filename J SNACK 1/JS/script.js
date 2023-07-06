6ee675fa465addd68bb3e56d427d9cfb604158e8
Antonio Piras <antonio.piras90@gmail.com>
2023-07-06 20:54:02 +0200

    lista  invitati stampata in pagina

--- J SNACK 1/JS/script.js
+++ J SNACK 1/JS/script.js
@@ -3,10 +3,10 @@
 // il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 
-const nomi = ["Antonio ","Marco ","Paolo ","Luca ","Stefano "];
-const cognomi = ["Galli ","Rossi ","Bianchi ","Stefanoni ","Belda "]
-
-
+const nomi = ["Antonio ","Marco ","Paolo ","Luca ","Stefano ", "Alessandro ","Giovanni ","Giorgio ","Simone" ];
+const cognomi = ["Galli ","Rossi ","Bianchi ","Stefanoni ","Belda ", "Gioffredi", "Ligresti","Belloni", "Bandelli"]
+const container = document.getElementById("container")
+const box = 10
 for (let i = 0; i < nomi.length && cognomi.length ; i++) {
 
    const randomName = nomi[ Math.floor(Math.random() * nomi.length)];
@@ -15,6 +15,11 @@ for (let i = 0; i < nomi.length && cognomi.length ; i++) {
     // console.log(randomSurname);
     const listaInvitati = randomName + "" + randomSurname;
     console.log(listaInvitati);
+    const box = document.createElement("div");
+   box.classList.add("child")
+   container.append(box)
+   box.innerHTML = listaInvitati
+    
 };
 
 
